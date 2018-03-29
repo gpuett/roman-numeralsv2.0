50988e73ffa072ed40138749cd56d227719e0bfd
Cory West-Forbes and Garnett Puett <cory@>
2018-03-29 09:42:37 -0700

    Parse out functions for less than 10, less than 50 and function that calls on each.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-// Business logic
+// Business logic --- Primary Function
 function romanNumeral(number) {
+  if (number < 10) {
+    return romanNumeralLessThanTen(number);
+  }
+}
+
+// Less than 10 Function
+function romanNumeralLessThanTen(number) {
   var romanNumeralString = "";
   if (number < 4) {
     for (var i = 1; i <= number; i++) {
@@ -7,21 +14,32 @@ function romanNumeral(number) {
     }
   } else if (number === 4) {
     romanNumeralString = "IV";
-  } else if (number === 5) {
+  }  else if (number === 5) {
     romanNumeralString = "V";
-  } else if (number === 10) {
-    romanNumeralString = "X";
-  } else if (number === 50) {
-    romanNumeralString = "L";
-  } else if (number === 100) {
-    romanNumeralString = "C";
-  } else if (number === 500) {
-    romanNumeralString = "D";
-  } else if (number === 1000) {
-    romanNumeralString = "M";
+  } else if (number > 5 && number < 9) {
+    romanNumeralString = "V";
+    for (var i = 6; i <= number; i++) {
+      romanNumeralString += "I";
+    }
+  } else if (number === 9) {
+    romanNumeralString = "IX"
   }
   return romanNumeralString
 }
+
+
+  // else if (number === 10) {
+  //   romanNumeralString = "X";
+  // } else if (number === 50) {
+  //   romanNumeralString = "L";
+  // } else if (number === 100) {
+  //   romanNumeralString = "C";
+  // } else if (number === 500) {
+  //   romanNumeralString = "D";
+  // } else if (number === 1000) {
+  //   romanNumeralString = "M";
+  // }
+
 
 
 
