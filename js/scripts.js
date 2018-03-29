3b046d4b6454b6c7ca0962ce7dd22f1cf43fe5ea
Cory West-Forbes and Garnett Puett <garnett@>
2018-03-29 16:46:26 -0700

    cut down code.

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,17 @@ function romanNumeral(number) {
       var elementNumber = parseInt(userRomanArray[i]);
         if (elementNumber < 4) {
         for (var j = 0; j < elementNumber; j++) {
-          outputRomanString += romanArray[0 + (2 * i)];
+          outputRomanString += romanArray[2 * i];
         }
       } else if (elementNumber >= 5 && elementNumber < 9) {
         for (var j = 0 ; j < elementNumber - 5; j++) {
-          outputRomanString += romanArray[0 + (2 * i)];
+          outputRomanString += romanArray[2 * i];
         }
         outputRomanString += romanArray[1 + (2 * i)];
       } else if (elementNumber === 4) {
-        outputRomanString += romanArray[1 + (2 * i)] + romanArray[0 + (2 * i)]
+        outputRomanString += romanArray[1 + (2 * i)] + romanArray[2 * i]
       } else if (elementNumber === 9) {
-        outputRomanString += romanArray[2 + (2 * i)] + romanArray[0 + (2 * i)]
+        outputRomanString += romanArray[2 + (2 * i)] + romanArray[2 * i]
       }
     }
   return outputRomanString.split("").reverse().join("");
