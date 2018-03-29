132edeb892fbfe3826b1659369b0cdfcf984de99
Cory West-Forbes and Garnett Puett <garnett@>
2018-03-29 10:27:00 -0700

    Add functionality for numbers less than 40.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 // Business logic --- Primary Function
 function romanNumeral(number) {
+  var romanNumeralString = "";
   if (number < 10) {
     return romanNumeralLessThanTen(number);
+  } else if (number < 40) {
+    return romanNumeralLessThanForty(number);
   }
+  return romanNumeralString
 }
 
 // Less than 10 Function
 function romanNumeralLessThanTen(number) {
-  var romanNumeralString = "";
   if (number < 4) {
     for (var i = 1; i <= number; i++) {
       romanNumeralString += "I"
@@ -27,9 +30,27 @@ function romanNumeralLessThanTen(number) {
   return romanNumeralString
 }
 
+function romanNumeralLessThanForty(number) {
+    if (number === 10) {
+      romanNumeralString = "X";
+    } else if (number > 10 && number < 20) {
+    romanNumeralString = "X";
+    romanNumeralString += romanNumeralLessThanTen(number-10)
+    console.log(romanNumeralString);
+  } else if (number === 20) {
+    romanNumeralString = "XX";
+  } else if (number > 20 && number < 30) {
+    romanNumeralString = "XX";
+    romanNumeralString += romanNumeralLessThanTen(number-20)
+  } else if (number === 30) {
+    romanNumeralString = "XXX";
+  } else if (number > 30 && number < 40) {
+    romanNumeralString = "XXX";
+    romanNumeralString += romanNumeralLessThanTen(number-30)
+  }
+  return romanNumeralString
 
-  // else if (number === 10) {
-  //   romanNumeralString = "X";
+}
   // } else if (number === 50) {
   //   romanNumeralString = "L";
   // } else if (number === 100) {
